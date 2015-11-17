8270fb620c4c35eb90d7d5d5d61b97bd16c758a0
dance2die <dontbothermewithspam@gmail.com>
2015-11-17 14:09:06 -0500

    Try & Except and also Input and If&Else

--- First.py
+++ First.py
@@ -3,6 +3,16 @@ def minmax(items):
 
 
 print(minmax([1,2,3,4,5,22,-1]))
-
 print(__name__)
+
+
+try:
+    value = int(input("Type 1 ~ 10:"))
+except ValueError:
+    print("1 ~ 10!")
+else:
+    if (value > 0) and (value <= 10):
+        print("You typed: {}".format(value))
+    else:
+        print("Wrong, try again!")
 
