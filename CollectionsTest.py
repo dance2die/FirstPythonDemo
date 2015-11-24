ee79a3a8e65a0a25d79dfcb6c4ba6633f353c58f
dance2die <dontbothermewithspam@gmail.com>
2015-11-24 11:57:41 -0500

    testing python set.

---
+++ CollectionsTest.py
@@ -0,0 +1,10 @@
+from pprint import pprint as pp
+
+developerSet = {"Sung", "Brendan", "Vasu"}
+helpDeskSet = {"Calvin", "Anthony"}
+itSet = developerSet.copy()
+
+
+pp(developerSet)
+pp(helpDeskSet)
+pp(itSet)
