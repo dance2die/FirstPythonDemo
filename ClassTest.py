0a70794672fae65339f1153d3bfc1e5f8efbc3ec
dance2die <dontbothermewithspam@gmail.com>
2015-12-05 14:09:51 -0500

    Learned about inheritance

--- ClassTest.py
+++ ClassTest.py
@@ -1,5 +1,6 @@
 from pprint import pprint as pp
 
+
 class Person:
     def __init__(self, name):
         self._name = name
@@ -16,9 +17,28 @@ class Person:
         self._name = value
 
 
+class Employee(Person):
+    def __init__(self, name, employeeId):
+        super().__init__(name)
+        self.employeeId = employeeId
+
+    @property
+    def employeeId(self):
+        return self._employeeId
+
+    @employeeId.setter
+    def employeeId(self, value):
+        self._employeeId = value
+
+
+
 def personPrinter(person):
     ''' To test Duck Typing '''
     pp("Name: {}".format(person.name))
+
+def employeePrinter(employee):
+    ''' To test Duck Typing '''
+    pp("Name: {}; EmployeeID: {}".format(employee.name, employee.employeeId))
 
 
 def main():
@@ -28,6 +48,10 @@ def main():
     p1.printPerson(personPrinter)
     p1._name = "Chris"
     p1.printPerson(personPrinter)
+    e1 = Employee("Employee1", 1)
+    e2 = Employee("Employee2", 2)
+    employeePrinter(e1)
+    employeePrinter(e2)
 
 if __name__ == '__main__':
     main()
