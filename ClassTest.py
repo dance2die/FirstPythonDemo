317941fed72e1e67afa880182235e203edae35fb
dance2die <dontbothermewithspam@gmail.com>
2015-12-04 19:55:58 -0500

    Learned about properties

--- ClassTest.py
+++ ClassTest.py
@@ -2,25 +2,31 @@ from pprint import pprint as pp
 
 class Person:
     def __init__(self, name):
-        self.setName(name)
-
-    def getName(self):
-        return self._name
-
-    def setName(self, name):
         self._name = name
 
     def printPerson(self, printer):
         printer(self)
 
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, value):
+        self._name = value
+
 
 def personPrinter(person):
     ''' To test Duck Typing '''
-    pp("Name: {}".format(person.getName()))
+    pp("Name: {}".format(person.name))
 
 
 def main():
     p1 = Person("Mike")
+    p1.printPerson(personPrinter)
+    p1.name = "John"
+    p1.printPerson(personPrinter)
+    p1._name = "Chris"
     p1.printPerson(personPrinter)
 
 if __name__ == '__main__':
