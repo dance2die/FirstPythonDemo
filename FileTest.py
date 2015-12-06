6018a947092f5d09b7401f9c36605f5b18a3745b
dance2die <dontbothermewithspam@gmail.com>
2015-12-06 16:00:01 -0500

    Learned about reading a file in Python

---
+++ FileTest.py
@@ -0,0 +1,14 @@
+from pprint import pprint as pp
+
+
+f = open("./First.py", mode='rt', encoding='utf-8')
+
+
+def main():
+    for line in f.readlines():
+        pp(line)
+
+if __name__ == '__main__':
+    main()
+
+
