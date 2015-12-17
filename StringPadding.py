bb38bcf288ee36cc2174f13cc59836647f5cae0c
dance2die <dontbothermewithspam@gmail.com>
2015-12-17 10:56:55 -0500

    more formatting

--- StringPadding.py
+++ StringPadding.py
@@ -9,3 +9,20 @@ pp('"aaa".rjust(10)={}'.format("aaa".rjust(10)))
 # pp("'{:*>}'.format('test')={}".format('{:*>}'.format('test')))
 # pp("'{:*^10}'.format('test')={}".format('{:*^10}'.format('test')))
 # pp("'{:*^10}'.format('test1')={}".format('{:*^10}'.format('test1')))
+
+Formatted = "{:d}"
+print(Formatted.format(7000))
+Formatted = "{:,d}"
+print(Formatted.format(7000))
+Formatted = "{:^15,d}"
+print(Formatted.format(7000))
+Formatted = "{:*^15,d}"
+print(Formatted.format(7000))
+Formatted = "{:*^15.2f}"
+print(Formatted.format(7000))
+Formatted = "{:*>15X}"
+print(Formatted.format(7000))
+Formatted = "{:*<#15x}"
+print(Formatted.format(7000))
+Formatted = "A {0} {1} and a {0} {2}."
+print(Formatted.format("blue", "car", "truck"))
