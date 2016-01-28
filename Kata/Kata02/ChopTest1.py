f3c3b772d746d0c72489adb6a753a3ea32a34e9b
dance2die <dontbothermewithspam@gmail.com>
2016-01-28 11:24:25 -0500

    created a unit test

--- Kata/Kata02/ChopTest1.py
+++ Kata/Kata02/ChopTest1.py
@@ -1,23 +1,31 @@
-def test_chop
-  assert_equal(-1, chop(3, []))
-  assert_equal(-1, chop(3, [1]))
-  assert_equal(0,  chop(1, [1]))
-  #
-  assert_equal(0,  chop(1, [1, 3, 5]))
-  assert_equal(1,  chop(3, [1, 3, 5]))
-  assert_equal(2,  chop(5, [1, 3, 5]))
-  assert_equal(-1, chop(0, [1, 3, 5]))
-  assert_equal(-1, chop(2, [1, 3, 5]))
-  assert_equal(-1, chop(4, [1, 3, 5]))
-  assert_equal(-1, chop(6, [1, 3, 5]))
-  #
-  assert_equal(0,  chop(1, [1, 3, 5, 7]))
-  assert_equal(1,  chop(3, [1, 3, 5, 7]))
-  assert_equal(2,  chop(5, [1, 3, 5, 7]))
-  assert_equal(3,  chop(7, [1, 3, 5, 7]))
-  assert_equal(-1, chop(0, [1, 3, 5, 7]))
-  assert_equal(-1, chop(2, [1, 3, 5, 7]))
-  assert_equal(-1, chop(4, [1, 3, 5, 7]))
-  assert_equal(-1, chop(6, [1, 3, 5, 7]))
-  assert_equal(-1, chop(8, [1, 3, 5, 7]))
-end
+import unittest
+
+
+# http://codekata.com/kata/kata02-karate-chop/
+class ChopTest1(unittest.TestCase):
+
+  def chop(self, index, array):
+    return 0
+
+  def test_chop(self):
+    self.assertEquals(-1, self.chop(3, []))
+    self.assertEquals(-1, self.chop(3, [1]))
+    self.assertEquals(0,  self.chop(1, [1]))
+    self.assertEquals(0,  self.chop(1, [1, 3, 5]))
+    self.assertEquals(1,  self.chop(3, [1, 3, 5]))
+    self.assertEquals(2,  self.chop(5, [1, 3, 5]))
+    self.assertEquals(-1, self.chop(0, [1, 3, 5]))
+    self.assertEquals(-1, self.chop(2, [1, 3, 5]))
+    self.assertEquals(-1, self.chop(4, [1, 3, 5]))
+    self.assertEquals(-1, self.chop(6, [1, 3, 5]))
+    self.assertEquals(0,  self.chop(1, [1, 3, 5, 7]))
+    self.assertEquals(1,  self.chop(3, [1, 3, 5, 7]))
+    self.assertEquals(2,  self.chop(5, [1, 3, 5, 7]))
+    self.assertEquals(3,  self.chop(7, [1, 3, 5, 7]))
+    self.assertEquals(-1, self.chop(0, [1, 3, 5, 7]))
+    self.assertEquals(-1, self.chop(2, [1, 3, 5, 7]))
+    self.assertEquals(-1, self.chop(4, [1, 3, 5, 7]))
+    self.assertEquals(-1, self.chop(6, [1, 3, 5, 7]))
+    self.assertEquals(-1, self.chop(8, [1, 3, 5, 7]))
+
+
